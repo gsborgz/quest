3f80df603d59553f48d72f09eb01d7ffef09eac1
gsborgz <borgesgabrielsilva@gmail.com>
2024-03-18 02:23:47 -0300

    creating `getServerStatus` method and adding error handlers

--- src/core/database.ts
+++ src/core/database.ts
@@ -1,6 +1,7 @@
 import { Client } from 'pg';
 
 type Database = {
+  getServerStatus(): Promise<DatabaseStatus>;
   query: (query: string) => Promise<any>;
   findOne: <T>() => Promise<T>;
   findAll: <T>() => Promise<T>;
@@ -16,63 +17,76 @@ const databaseConfig = {
   database: process.env.POSTGRES_DB
 };
 
+export type DatabaseStatus = {
+  version: string;
+  max_connections: number;
+  opened_connections: number;
+}
+
 const database: Database = {
+  async getServerStatus(): Promise<DatabaseStatus> {
+    const client = new Client(databaseConfig);
+
+    try {
+      await client.connect();
+
+      const serverVersion = await client.query('SHOW server_version;');
+      const serverMaxConnections = await client.query('SHOW max_connections;');
+      const serverOpenedConnections = await client.query({
+        text: 'SELECT count(*)::int from pg_stat_activity WHERE datname = $1;',
+        values: [process.env.POSTGRES_DB]
+      });
+
+      const body: DatabaseStatus = {
+        version: serverVersion.rows[0].server_version,
+        max_connections: Number(serverMaxConnections.rows[0].max_connections),
+        opened_connections: serverOpenedConnections.rows[0].count
+      };
+
+      return body;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await client.end();
+    }
+  },
+
   async query(query: string): Promise<any> {
     const client = new Client(databaseConfig);
 
-    await client.connect();
+    try {
+      await client.connect();
 
-    const result = await client.query(query);
+      const result = await client.query(query);
 
-    await client.end();
+      return result;
+    } catch (error) {
 
-    return result;
+    } finally {
+      await client.end();
+    }
   },
 
   async findOne<T>(): Promise<T> {
-    const client = new Client(databaseConfig);
-
-    await client.connect();
-
-    const result = await client.query('');
-
-    await client.end();
+    const result = await this.query('');
 
     return result as any;
   },
 
   async findAll<T>(): Promise<T> {
-    const client = new Client(databaseConfig);
-
-    await client.connect();
-
-    const result = await client.query('');
-
-    await client.end();
+    const result = await this.query('');
 
     return result as any;
   },
 
   async save<T>(): Promise<T> {
-    const client = new Client(databaseConfig);
-
-    await client.connect();
-
-    const result = await client.query('');
-
-    await client.end();
+    const result = await this.query('');
 
     return result as any;
   },
 
   async delete(): Promise<any> {
-    const client = new Client(databaseConfig);
-
-    await client.connect();
-
-    const result = await client.query('');
-
-    await client.end();
+    const result = await this.query('');
 
     return result;
   }
