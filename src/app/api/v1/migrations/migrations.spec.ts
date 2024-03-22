fe295af636aeb7985c79839cb1769becffac400f
gsborgz <borgesgabrielsilva@gmail.com>
2024-03-22 01:06:25 -0300

    adding `POST` method for `migrations` endpoint

--- src/app/api/v1/migrations/migrations.spec.ts
+++ src/app/api/v1/migrations/migrations.spec.ts
@@ -8,4 +8,14 @@ describe('api/v1/migrations', () => {
       expect(Array.isArray(responseBody)).toBe(true);
     });
   });
+
+  describe('POST', () => {
+    it.only('should return 200', async () => {
+      const response = await fetch('http://localhost:3000/api/v1/migrations', { method: 'POST' });
+      const responseBody = await response.json();
+
+      expect(response.status).toBe(200);
+      expect(Array.isArray(responseBody)).toBe(true);
+    });
+  });
 });
--- src/app/api/v1/migrations/route.ts
+++ src/app/api/v1/migrations/route.ts
@@ -1,12 +1,26 @@
-import migrationRunner from 'node-pg-migrate';
+import migrationRunner, { RunnerOption } from 'node-pg-migrate';
+import { join } from 'node:path';
+
+const migrationRunnerOptions: RunnerOption = {
+  databaseUrl: process.env.DATABASE_URL,
+  dir: join('infra', 'migrations'),
+  migrationsTable: 'pgmigrations',
+  direction: 'up'
+};
 
 export async function GET() {
   const migrations = await migrationRunner({
-    databaseUrl: process.env.DATABASE_URL,
-    dir: 'migrations',
-    migrationsTable: 'migrations',
-    direction: 'up',
+    ...migrationRunnerOptions,
     dryRun: true
+  });
+
+  return Response.json(migrations)
+}
+
+export async function POST() {
+  const migrations = await migrationRunner({
+    ...migrationRunnerOptions,
+    dryRun: false
   });
 
   return Response.json(migrations)
